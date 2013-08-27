660e3a457a628f1ba1fad083ad854f3f2d0730e0
Ethan Chen <intervigil@gmail.com>
2013-08-27 14:17:01 -0700

    exynos5: only build for insignal SLSI variant
    Change-Id: I1a9925b75188eec7932a72396d727c6741d3dc9f

--- libhwc_tvprimary/Android.mk
+++ libhwc_tvprimary/Android.mk
@@ -16,6 +16,9 @@ LOCAL_PATH:= $(call my-dir)
 # HAL module implemenation, not prelinked and stored in
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.product.board>.so
 include $(CLEAR_VARS)
+
+PLATFORM_DIR := $(TARGET_BOARD_PLATFORM)-insignal
+
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libEGL libGLESv1_CM libhardware \
